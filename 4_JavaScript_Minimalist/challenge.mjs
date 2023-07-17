c0818e8b0cb94bde41bb0427b00cdbf557735a60
Stuebinger, Bernd <stuebingerb@mediamarktsaturn.com>
2023-07-17 15:02:09 +0200

    Reduce size of JS solution

--- 4_JavaScript_Minimalist/challenge.mjs
+++ 4_JavaScript_Minimalist/challenge.mjs
@@ -1,1 +1,1 @@
-export{weightedSort};let weightedSort=(t)=>[...new Set(t.replace(/['-]/g,"").split(/[ .,!?]/).filter(n=>n))].map(w=>new Array(w,[...w].reduce((p,c)=>p+=c.charCodeAt(),0))).sort().reverse().sort((a,b)=>b[1]-a[1]).map(e=>e[0]+"|"+e[1]).join`\n`;
+export{weightedSort};let weightedSort=(t)=>[...new Set(t.replace(/['-]/g,"").split(/[ .,!?]/).filter(n=>n))].map(w=>[w,[...w].reduce((p,c)=>p+=c.charCodeAt(),0)]).sort().reverse().sort((a,b)=>b[1]-a[1]).map(e=>e[0]+"|"+e[1]).join`\n`;
