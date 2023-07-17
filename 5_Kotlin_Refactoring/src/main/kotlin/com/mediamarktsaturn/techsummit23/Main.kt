e368c2934809290f28020e8002d505981bbd65e8
Stuebinger, Bernd <stuebingerb@mediamarktsaturn.com>
2023-07-17 16:45:26 +0200

    First attempt at the Kotlin refactoring challenge

--- 5_Kotlin_Refactoring/src/main/kotlin/com/mediamarktsaturn/techsummit23/Main.kt
+++ 5_Kotlin_Refactoring/src/main/kotlin/com/mediamarktsaturn/techsummit23/Main.kt
@@ -1,72 +1,40 @@
 package com.mediamarktsaturn.techsummit23
 
 import org.apache.hc.core5.http.HttpResponse
-import java.io.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.ObjectOutputStream
+import java.io.Serializable
 import java.util.Scanner
 
-fun Serializable.toByteArray(): ByteArray {
-    val byteArrayOutputStream = ByteArrayOutputStream()
-    var objectOutputStream: ObjectOutputStream? = null
-    try {
-        objectOutputStream = ObjectOutputStream(byteArrayOutputStream)
-        objectOutputStream.writeObject(this)
-        objectOutputStream.flush()
-        val result = byteArrayOutputStream.toByteArray()
-        return result
-    } finally {
-        if (objectOutputStream != null) {
-            objectOutputStream.close()
-        }
-        byteArrayOutputStream.close()
+fun Serializable.toByteArray() = ByteArrayOutputStream().use { bos ->
+    ObjectOutputStream(bos).use {
+        it.writeObject(this)
+        it.flush()
+        bos.toByteArray()
     }
 }
 
-fun handleResponse(response: HttpResponse?): String {
-    if (response == null) {
-        throw IllegalStateException("Response is null")
+fun handleResponse(response: HttpResponse?): String = response?.run {
+    when {
+        code in 200..201 -> reasonPhrase
+        code in 400..499 -> throw IllegalStateException("Server responses with client error")
+        code in 500..599 -> throw IllegalStateException("Server responses with server error")
+        else -> throw IllegalStateException("Receive unexpected response code " + code)
     }
-
-    val code: Int = response.code
-    if (code == 200 || code == 201) {
-        return response.reasonPhrase
-    }
-
-    if (code >= 400 && code <= 499) {
-        throw IllegalStateException("Server responses with client error")
-    }
-
-    if (code >= 500 && code <= 599) {
-        throw IllegalStateException("Server responses with server error")
-    }
-
-    throw IllegalStateException("Receive unexpected response code " + code)
-}
+} ?: throw IllegalStateException("Response is null")
 
 private const val DELIMITER = "[^a-zA-Z'äöü]+"
 
-fun File.countWord(): MutableMap<String, Int> {
-    var scanner: Scanner? = null
-    try {
-        scanner = Scanner(this)
-        scanner.useDelimiter(DELIMITER)
-        val wordCount: MutableMap<String, Int> = mutableMapOf<String, Int>()
-        while (scanner.hasNext()) {
-            val word: String = scanner.next()
-            if (!wordCount.containsKey(word)) {
-                wordCount.put(word, 1)
-            } else {
-                val currentCount = wordCount.get(word)
-                if (currentCount != null) {
-                    wordCount.put(word, currentCount + 1)
-                }
-            }
-        }
-        return wordCount
-    } catch (e: IOException) {
-        throw IllegalArgumentException("Unable to read the current file ${this.name}!", e)
-    } finally {
-        if (scanner != null) {
-            scanner.close()
-        }
+fun File.countWord(): Map<String, Int> = runCatching {
+    Scanner(this).use {
+        it.useDelimiter(DELIMITER)
+        val words = mutableListOf<String>()
+        while (it.hasNext()) {
+            words.add(it.next())
+        }    
+        words.groupingBy { it }.eachCount()
     }
+}.getOrElse {
+    throw IllegalArgumentException("Unable to read the current file ${this.name}!", it)
 }
